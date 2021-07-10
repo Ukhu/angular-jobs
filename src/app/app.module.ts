773b1d76b85a161fc5893b77b61d95fd44c73c7b
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2021-07-10 16:21:41 +0100

    build the job card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JobCardComponent } from './job-card/job-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobCardComponent
   ],
   imports: [
     BrowserModule
